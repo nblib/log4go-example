4c3e90bcf1146f9c370340c44f8f40b40d7f1b6a
hewe <272489473@qq.com>
2018-01-18 10:21:17 +0800

    v1.1.dev 的demo

--- demo.go
+++ demo.go
@@ -1,21 +1,33 @@
 package main
 
 import (
-	"github.com/nblib/log4go-example/demo1"
 	"github.com/nblib/log4go-example/demo2"
 	"time"
+	"fmt"
 )
 
 func main() {
 
-	demo1 := demo1.Demo1{}
+	//demo1 := demo1.Demo1{}
 	//for i := 0; i < 10000; i++ {
-	demo1.Out()
+	//demo1.Out()
 	//}
 
+	/*for i := 0; i < 1000; i++ {
+		go func() {
+			dem2 := demo2.Demo2{}
+			for true {
+				dem2.Out()
+
+			}
+		}()
+	}*/
 	dem2 := demo2.Demo2{}
-	//for i := 0; i < 10000; i++ {
-	dem2.Out()
-	//}
-	time.Sleep(1 * time.Second)
+	for true {
+
+		dem2.Out()
+		fmt.Println(".")
+	}
+
+	time.Sleep(200 * time.Second)
 }
